e1d2595789066285c57414af19b2537af30d40f5
rushin236 <rushikeshshindesandesh@gmai.com>
2023-09-23 17:19:20 +0530

    build setup for project

---
+++ setup.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+HYPHN_EDOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirments
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHN_EDOT in requirements:
+            requirements.remove(HYPHN_EDOT)
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Rushikesh",
+    author_email="rushikeshshindesandesh@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
